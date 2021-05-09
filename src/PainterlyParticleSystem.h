a40a1f40ace1f7675a8fb36c4b706773f36449c1
Lauren <lc5313@rit.edu>
2021-05-09 15:20:20 -0400

    why

--- src/PainterlyParticleSystem.h
+++ src/PainterlyParticleSystem.h
@@ -1,5 +1,37 @@
+#include <glm/glm.hpp>
+#include "glm/gtc/matrix_transform.hpp"
+#include <glm/common.hpp>
+#include "VertexArray.h"
+#include "VertexBufferLayout.h"
+#include "IndexBuffer.h"
+#include "Renderer.h"
+#include "Random.h"
+#include "ComputeShader.h"
+#include "ShaderStorageBufferObject.h"
+#include "Texture.h"
+#include <vector>
+#include <algorithm>
+#include <array>
+//#include <vendor\glm\ext\quaternion_common.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #pragma once
+
+struct Particle {
+	glm::vec4 position;
+};
+
+
 class PainterlyParticleSystem
 {
+private:
+	VertexArray va;
+	VertexBuffer vb;
+	IndexBuffer ib;
+	VertexBufferLayout layout;
+	Shader shader;
+	ComputeShader cShader;
+	public:
+		PainterlyParticleSystem(int size, std::string vfshaderfilepath, std::string computefilepath);
 };
 
